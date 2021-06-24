c13f5d56cc874d2549e0948d89c3c5652de684ea
Alexandr Shatokhin <shatokhin.04@inbox.ru>
2021-06-24 22:05:21 +0300

    few changes v2

--- dist/js/script.js
+++ dist/js/script.js
@@ -80,7 +80,7 @@ $('#myForm').validate({
     e.preventDefault();
     $.ajax({
         type: "POST",
-        url: "../mailer/smart.php",
+        url: "mailer/smart.php",
         data: $(this).serialize()
     }).done(function() {
         $(this).find("input").val("");
